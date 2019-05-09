8cbfdaab1f822fce4f37f2fd244fedf8a54e3116
Matthew Smith <matthewsmith@MacBook.home>
2019-05-09 19:25:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World
+          Hello World 3
         </p>
         <a
           className="App-link"
